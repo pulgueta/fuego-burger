53aeed2a224adab4727b6f011fcc10b355a2764b
pulgueta <retardix456@gmail.com>
2024-12-06 02:59:59 -0500

    chore: add colors, font and biome

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "fuego-red": "#FF0000",
+        "fuego-orange": "#FFA500",
+        "fuego-yellow": "#FFD700",
+      },
+      fontFamily: {
+        display: ["Oswald", "sans-serif"],
+        body: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
